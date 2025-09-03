cf90aae06a1c24254c31c8f9486cc31e022d5e29
Bianca Caetano <bruninhakjurugat11@gmail.com>
2025-09-03 20:50:54 -0300

    update common module to export database module and integrate it into app module

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forRoot('mongodb+srv://127.0.0.1/sleepr')],
+})
 export class DatabaseModule {}
